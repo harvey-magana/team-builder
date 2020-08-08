6f602ed5fd68eb8d58e36696008180148567fef2
harvey magana <heyharv2012@gmail.com>
2020-08-07 18:51:31 -0700

    Added state to App component.

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TeamForm from './components/TeamForm';
 import TeamDisplay from './components/TeamDisplay';
 import './App.css';
 
 function App() {
+  const [ teams, setTeams ] = useState([
+    {
+      id: 1, 
+      name: "Jeff", 
+      role: "Actor"
+    }
+  ])
+
   return (
     <div className="App">
         <TeamForm />
