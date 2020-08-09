fe9a7886b4a92051568283ea80e514de0810e826
harvey magana <heyharv2012@gmail.com>
2020-08-09 14:59:13 -0700

    Using styled components to style the form.

--- team-builder/src/components/TeamDisplay.js
+++ team-builder/src/components/TeamDisplay.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
 import '../App.css';
 
 const TeamDisplay = (props) => {
--- team-builder/src/components/TeamForm.js
+++ team-builder/src/components/TeamForm.js
@@ -1,5 +1,47 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import '../App.css';
+
+const FormGroup = styled.div`
+	color: palevioletred;
+    display: block;
+	width: 300px;
+	margin: 50px auto;
+`;
+
+const Label = styled.label`
+	margin-bottom: 0.5em;
+	color: palevioletred;
+    display: block;
+`;
+
+
+const Input = styled.input`
+	padding: 0.5em;
+	color: palevioletred;
+	background: papayawhip;
+	border: none;
+	border-radius: 3px;
+	width: 100%;
+	margin-bottom: 0.5em;
+`;
+
+const Message = styled.label`
+	margin-bottom: 0.5em;
+	color: palevioletred;
+    display: block;
+`;
+
+const Button = styled.button`
+  /* Adapt the colors based on primary prop */
+  background: ${props => props.primary ? "palevioletred" : "white"};
+  color: ${props => props.primary ? "white" : "palevioletred"};
+
+  font-size: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
 
 const TeamForm = (props) => {
   const [ members, setMembers ] = useState({
@@ -23,21 +65,16 @@ const TeamForm = (props) => {
 
   return (
     <div className="App">
-        <form onSubmit={submitForm}>
-            <label>
-                Name:
-                <input name="name" onChange={onLineChange} />
-            </label>
-            <label>
-                Email:
-                <input name="email" onChange={onLineChange} />
-            </label>
-            <label>
-                Role:
-                <input name="role" onChange={onLineChange} />
-            </label>
-            <button type="submit">Submit</button>
-        </form>
+        <FormGroup onSubmit={submitForm}>
+        <Label htmlFor="label">Name</Label>
+        <Input id="label" name="name" onChange={onLineChange} />
+        <Label>Email</Label>
+        <Input name="email" onChange={onLineChange} />
+        <Label>Role</Label>
+        <Input name="role" onChange={onLineChange} />
+        <Message>This is the validation message</Message>
+        <Button type="submit" primary>Primary</Button>
+        </FormGroup>
     </div>
   );
 }
